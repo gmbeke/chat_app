78719840ad7d12b4490f78e4946e3a3d94578f4c
Muhidin-Hassani <muhidinhassani5@gmail.com>
2026-02-19 09:34:29 +0300

    Update online_indicator.dart

--- lib/widgets/online_indicator.dart
+++ lib/widgets/online_indicator.dart
@@ -1,21 +1,67 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class OnlineIndicator extends StatelessWidget {
+class OnlineIndicator extends StatefulWidget {
   final bool isOnline;
   final double size;
 
-  const OnlineIndicator({super.key, required this.isOnline, this.size = 12});
+  const OnlineIndicator({
+    super.key,
+    required this.isOnline,
+    this.size = 12,
+  });
+
+  @override
+  State<OnlineIndicator> createState() => _OnlineIndicatorState();
+}
+
+class _OnlineIndicatorState extends State<OnlineIndicator> {
+  int secondsOffline = 0;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (!widget.isOnline) {
+      startTimer();
+    }
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (t) {
+      setState(() {
+        secondsOffline++;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        color: isOnline ? Colors.green : Colors.grey,
-        shape: BoxShape.circle,
-        border: Border.all(color: Colors.white, width: 2),
-      ),
+    return Column(
+      children: [
+        Container(
+          width: widget.size,
+          height: widget.size,
+          decoration: BoxDecoration(
+            color: widget.isOnline ? Colors.green : Colors.grey,
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white, width: 2),
+          ),
+        ),
+
+        if (!widget.isOnline)
+          Text(
+            '${secondsOffline}s ago',
+            style: const TextStyle(fontSize: 10),
+          ),
+      ],
     );
   }
 }
